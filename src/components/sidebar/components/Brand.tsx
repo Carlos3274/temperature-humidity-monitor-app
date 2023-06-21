39707fa690f61fe9be80afc39d9e23fd450afb41
José Carlos <jose.carlos.araujo07@aluno.ifce.edu.br>
2023-06-21 10:52:49 -0300

    :sparkles: components cleanded

--- src/components/sidebar/components/Brand.tsx
+++ src/components/sidebar/components/Brand.tsx
@@ -1,20 +1,21 @@
 // Chakra imports
-import { Flex, useColorModeValue } from '@chakra-ui/react';
+import { Flex, Heading, useColorModeValue } from '@chakra-ui/react'
 
 // Custom components
-import { HorizonLogo } from 'components/icons/Icons';
-import { HSeparator } from 'components/separator/Separator';
+import { HSeparator } from 'components/separator/Separator'
 
 export function SidebarBrand() {
-	//   Chakra color mode
-	let logoColor = useColorModeValue('navy.700', 'white');
+  //   Chakra color mode
+  let logoColor = useColorModeValue('navy.700', 'white')
 
-	return (
-		<Flex alignItems='center' flexDirection='column'>
-			<HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
-			<HSeparator mb='20px' />
-		</Flex>
-	);
+  return (
+    <Flex alignItems="center" flexDirection="column" justifyContent="center">
+      <Heading mb="32px" fontSize="4xl">
+        IoT Monitor
+      </Heading>
+      <HSeparator mb="20px" />
+    </Flex>
+  )
 }
 
-export default SidebarBrand;
+export default SidebarBrand
