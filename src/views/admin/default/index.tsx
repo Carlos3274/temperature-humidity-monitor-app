d03c2f82a98e2fc436f9f89ae1713b2043ebfad6
José Carlos <jose.carlos.araujo07@aluno.ifce.edu.br>
2023-06-21 15:15:32 -0300

    feat: first line done

--- src/views/admin/default/index.tsx
+++ src/views/admin/default/index.tsx
@@ -21,116 +21,174 @@
 */
 
 // Chakra imports
-import { Avatar, Box, Flex, FormLabel, Icon, Select, SimpleGrid, useColorModeValue } from '@chakra-ui/react';
+import { Box, Icon, SimpleGrid, useColorModeValue } from '@chakra-ui/react'
 // Assets
-import Usa from 'assets/img/dashboards/usa.png';
 // Custom components
-import MiniCalendar from 'components/calendar/MiniCalendar';
-import MiniStatistics from 'components/card/MiniStatistics';
-import IconBox from 'components/icons/IconBox';
-import { MdAddTask, MdAttachMoney, MdBarChart, MdFileCopy } from 'react-icons/md';
-import CheckTable from 'views/admin/rtl/components/CheckTable';
-import ComplexTable from 'views/admin/default/components/ComplexTable';
-import DailyTraffic from 'views/admin/default/components/DailyTraffic';
-import PieCard from 'views/admin/default/components/PieCard';
-import Tasks from 'views/admin/default/components/Tasks';
-import TotalSpent from 'views/admin/default/components/TotalSpent';
-import WeeklyRevenue from 'views/admin/default/components/WeeklyRevenue';
-import tableDataCheck from 'views/admin/default/variables/tableDataCheck';
-import tableDataComplex from 'views/admin/default/variables/tableDataComplex';
+import MiniCalendar from 'components/calendar/MiniCalendar'
+import MiniStatistics from 'components/card/MiniStatistics'
+import IconBox from 'components/icons/IconBox'
+import { MdOutlineWaterDrop } from 'react-icons/md'
+import {
+  TbTemperature,
+  TbTemperatureMinus,
+  TbTemperaturePlus
+} from 'react-icons/tb'
+import ComplexTable from 'views/admin/default/components/ComplexTable'
+import DailyTraffic from 'views/admin/default/components/DailyTraffic'
+import PieCard from 'views/admin/default/components/PieCard'
+import Tasks from 'views/admin/default/components/Tasks'
+import TotalSpent from 'views/admin/default/components/TotalSpent'
+import WeeklyRevenue from 'views/admin/default/components/WeeklyRevenue'
+import tableDataCheck from 'views/admin/default/variables/tableDataCheck'
+import tableDataComplex from 'views/admin/default/variables/tableDataComplex'
+import CheckTable from 'views/admin/rtl/components/CheckTable'
 
 export default function UserReports() {
-	// Chakra Color Mode
-	const brandColor = useColorModeValue('brand.500', 'white');
-	const boxBg = useColorModeValue('secondaryGray.300', 'whiteAlpha.100');
-	return (
-		<Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
-			<SimpleGrid columns={{ base: 1, md: 2, lg: 3, '2xl': 6 }} gap='20px' mb='20px'>
-				<MiniStatistics
-					startContent={
-						<IconBox
-							w='56px'
-							h='56px'
-							bg={boxBg}
-							icon={<Icon w='32px' h='32px' as={MdBarChart} color={brandColor} />}
-						/>
-					}
-					name='Earnings'
-					value='$350.4'
-				/>
-				<MiniStatistics
-					startContent={
-						<IconBox
-							w='56px'
-							h='56px'
-							bg={boxBg}
-							icon={<Icon w='32px' h='32px' as={MdAttachMoney} color={brandColor} />}
-						/>
-					}
-					name='Spend this month'
-					value='$642.39'
-				/>
-				<MiniStatistics growth='+23%' name='Sales' value='$574.34' />
-				<MiniStatistics
-					endContent={
-						<Flex me='-16px' mt='10px'>
-							<FormLabel htmlFor='balance'>
-								<Avatar src={Usa} />
-							</FormLabel>
-							<Select id='balance' variant='mini' mt='5px' me='0px' defaultValue='usd'>
-								<option value='usd'>USD</option>
-								<option value='eur'>EUR</option>
-								<option value='gba'>GBA</option>
-							</Select>
-						</Flex>
-					}
-					name='Your balance'
-					value='$1,000'
-				/>
-				<MiniStatistics
-					startContent={
-						<IconBox
-							w='56px'
-							h='56px'
-							bg='linear-gradient(90deg, #4481EB 0%, #04BEFE 100%)'
-							icon={<Icon w='28px' h='28px' as={MdAddTask} color='white' />}
-						/>
-					}
-					name='New Tasks'
-					value='154'
-				/>
-				<MiniStatistics
-					startContent={
-						<IconBox
-							w='56px'
-							h='56px'
-							bg={boxBg}
-							icon={<Icon w='32px' h='32px' as={MdFileCopy} color={brandColor} />}
-						/>
-					}
-					name='Total Projects'
-					value='2935'
-				/>
-			</SimpleGrid>
+  // Chakra Color Mode
+  const brandColor = useColorModeValue('brand.500', 'white')
+  const boxBg = useColorModeValue('secondaryGray.300', 'whiteAlpha.100')
+  return (
+    <Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
+      <SimpleGrid
+        columns={{ base: 1, md: 2, lg: 3, '2xl': 6 }}
+        gap="20px"
+        mb="20px"
+      >
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={
+                <Icon w="32px" h="32px" as={TbTemperature} color={brandColor} />
+              }
+            />
+          }
+          name="Temperatura Atual"
+          value="30 °C"
+        />
+        <MiniStatistics
+          growth="+23%"
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={
+                <Icon
+                  w="32px"
+                  h="32px"
+                  as={TbTemperatureMinus}
+                  color={brandColor}
+                />
+              }
+            />
+          }
+          name="Temperatura Min"
+          value="29 °C"
+        />
 
-			<SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px' mb='20px'>
-				<TotalSpent />
-				<WeeklyRevenue />
-			</SimpleGrid>
-			<SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap='20px' mb='20px'>
-				<CheckTable tableData={tableDataCheck} />
-				<SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px'>
-					<DailyTraffic />
-					<PieCard />
-				</SimpleGrid>
-			</SimpleGrid>
-			<SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap='20px' mb='20px'>
-				<ComplexTable tableData={tableDataComplex} />
-				<SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px'>
-					<Tasks />
-					<MiniCalendar h='100%' minW='100%' selectRange={false} />
-				</SimpleGrid>
-			</SimpleGrid>
-		</Box>
-	);
+        <MiniStatistics
+          growth="+10%"
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={
+                <Icon
+                  w="32px"
+                  h="32px"
+                  as={TbTemperaturePlus}
+                  color={brandColor}
+                />
+              }
+            />
+          }
+          name="Temperatura Máx"
+          value="31 °C"
+        />
+
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={
+                <Icon
+                  w="32px"
+                  h="32px"
+                  as={MdOutlineWaterDrop}
+                  color={brandColor}
+                />
+              }
+            />
+          }
+          name="Umidade"
+          value="60%"
+        />
+
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={
+                <Icon
+                  w="32px"
+                  h="32px"
+                  as={MdOutlineWaterDrop}
+                  color={brandColor}
+                />
+              }
+            />
+          }
+          name="Umidade Min"
+          value="40%"
+        />
+        <MiniStatistics
+          growth={'10%'}
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={
+                <Icon
+                  w="32px"
+                  h="32px"
+                  as={MdOutlineWaterDrop}
+                  color={brandColor}
+                />
+              }
+            />
+          }
+          name="Umidade Máx"
+          value="65%"
+        />
+      </SimpleGrid>
+
+      <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap="20px" mb="20px">
+        <TotalSpent />
+        <WeeklyRevenue />
+      </SimpleGrid>
+      <SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap="20px" mb="20px">
+        <CheckTable tableData={tableDataCheck} />
+        <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap="20px">
+          <DailyTraffic />
+          <PieCard />
+        </SimpleGrid>
+      </SimpleGrid>
+      <SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap="20px" mb="20px">
+        <ComplexTable tableData={tableDataComplex} />
+        <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap="20px">
+          <Tasks />
+          <MiniCalendar h="100%" minW="100%" selectRange={false} />
+        </SimpleGrid>
+      </SimpleGrid>
+    </Box>
+  )
 }
